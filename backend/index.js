ace3cfa8fa1e0d9c904be477f1d467eeccefe731
andersonPaez <anderson.paez.1412@gmail.com>
2022-10-14 14:41:21 -0500

    avance frontend

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,30 @@
-const ClienteModelo = require("./models/cliente");
+// const ClienteModelo = require("./models/cliente");
 
-const uri = "mongodb://localhost:27017/adminservice";
+// const uri = "mongodb://localhost:27017/adminservice";
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(uri).then(() => {
-    console.log("Conexion exitosa.");
-}).catch(() => {
-    console.log("Falla en la conexion.");
-});
+// mongoose.connect(uri).then(() => {
+//     console.log("Conexion exitosa.");
+// }).catch(() => {
+//     console.log("Falla en la conexion.");
+// });
 
-console.log("Conectando a la BD.");
+// console.log("Conectando a la BD.");
 
-// registro de ejemplo.
-const cliente1 = {
-    tipoDocumento : "CC",
-    identificacion : "1065903852",
-    nombre : "Anderson",
-    apellido : "Paez",
-    email : "correo@gmail.com",
-    telefono : "1234567899",
-    password : "contrasenia"
-}
+// // registro de ejemplo.
+// const cliente1 = {
+//     tipoDocumento : "CC",
+//     identificacion : "1065903852",
+//     nombre : "Anderson",
+//     apellido : "Paez",
+//     email : "correo@gmail.com",
+//     telefono : "1234567899",
+//     password : "contrasenia"
+// }
 
-ClienteModelo.create(cliente1).then((resultado)=>{
-    console.log(`Se inserto el cliente ${resultado.nombre}`);
-}).catch(()=>{
-    console.log("Error al insertar cliente.");
-});
+// ClienteModelo.create(cliente1).then((resultado)=>{
+//     console.log(`Se inserto el cliente ${resultado.nombre}`);
+// }).catch(()=>{
+//     console.log("Error al insertar cliente.");
+// });
